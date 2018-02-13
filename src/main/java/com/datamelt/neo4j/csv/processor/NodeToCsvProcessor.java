75ef148bbcef8d12075129af9cc54d7d55620d0f
uwegeercken <uwe.geercken@web.de>
2018-02-13 23:28:53 +0100

    added licence info

--- src/main/java/com/datamelt/neo4j/csv/processor/NodeToCsvProcessor.java
+++ src/main/java/com/datamelt/neo4j/csv/processor/NodeToCsvProcessor.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.datamelt.neo4j.csv.processor;
 
 import java.io.BufferedReader;
@@ -132,9 +151,9 @@ public class NodeToCsvProcessor
 	}
 	
 	/** 
-     * returns the version and revision of the business rule engine
+     * returns the version and revision of the program
      * 
-     * @return		the version and revision of the engine
+     * @return		the version and revision of the program
      */
     public static String getVersion()
     {
@@ -142,7 +161,7 @@ public class NodeToCsvProcessor
     }
     
     /** 
-     * returns the last date of an update of the business rule engine
+     * returns the last date of an update of the program
      * 
      * @return		the last updated date
      */
@@ -163,7 +182,7 @@ public class NodeToCsvProcessor
     	System.out.println("      [password]     : required. password of the neo4j user.");
     	System.out.println("      [outputfolder] : required. path of the folder where to generate the files.");
     	System.out.println("      [csv file name]: required. name of the csv file - containing the data - to use");
-    	System.out.println("      [delimiter]    : optional. field delimiter to use for the generates files");
+    	System.out.println("      [delimiter]    : optional. default:\";\".field delimiter to use for the generates files");
     	System.out.println();
     	System.out.println("example: NodeToCsvProcessor -h=localhost -u=user1 -p=mypassword -o=/home/user1 -c=/home/user1/testfile.csv -d=;");
     	System.out.println();
